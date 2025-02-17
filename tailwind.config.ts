5c297544b43713ffc920d28aa98f75b485799f22
Yash-KK <reachyashk@gmail.com>
2025-02-17 23:39:15 +0530

    add quote component for auth pages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        bubblegum: ['Bubblegum Sans', 'sans-serif'],
+      },
     },
   },
   plugins: [],
