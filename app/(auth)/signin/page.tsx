e647dd23895584cd769bd39af46f10188c7c0474
Yash-KK <reachyashk@gmail.com>
2025-02-22 16:54:17 +0530

    create sign-in component

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,19 +1,15 @@
 import Quote from "@/components/Quote";
-
+import SignInForm from "@/components/Auth/SignIn";
 export default function SignIn() {
   return (
-    <>
-      <div className="grid grid-cols-2">
-        <div className="flex justify-center items-center h-screen">
-          Sign In Form
-        </div>
-        <div>
-          <Quote
-            text={`Back to relive the glory days? Or just here to cringe at old memories? Either way, welcome back to Memoria!`}
-            size="md"
-          />
-        </div>
+    <div className="grid grid-cols-2">
+      <SignInForm />
+      <div>
+        <Quote
+          text={`Back to relive the glory days? Or just here to cringe at old memories? Either way, welcome back to Memoria!`}
+          size="md"
+        />
       </div>
-    </>
+    </div>
   );
 }
