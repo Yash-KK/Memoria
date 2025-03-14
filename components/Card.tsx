e4019a874c8c36ed5b2bb9e4b7721f885c26a3f9
Yash-KK <reachyashk@gmail.com>
2025-03-14 18:09:27 +0530

    add delete functionality

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,15 +3,29 @@ import { embedYoutubeUrl } from "@/lib/utils";
 import { DeleteIcon, ShareIcon, TweetIcon, VideoIcon } from "./icons";
 import Label from "./ui/Label";
 import { Tweet } from "react-tweet";
+import Modal from "./Modal";
+import { useState } from "react";
 
 type CardProps = {
+  contentId: string;
   type: "Youtube" | "Twitter";
   title: string;
   link: string;
   createdAt: Date;
 };
-const Card: React.FC<CardProps> = ({ title, type, link, createdAt }) => {
+const Card: React.FC<CardProps> = ({
+  contentId,
+  title,
+  type,
+  link,
+  createdAt,
+}) => {
   const twitterId = type === "Twitter" && link.split("/").at(-1);
+
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const handleModalToggle = () => {
+    setModalOpen((prev) => !prev);
+  };
   return (
     <div className="flex m-5 bg-gray-800  hover:bg-gray-700 text-white flex-col max-w-sm rounded h-96 shadow-lg px-6 py-4">
       <div className="flex justify-between items-center">
@@ -32,7 +46,9 @@ const Card: React.FC<CardProps> = ({ title, type, link, createdAt }) => {
 
         <div className="flex items-center gap-x-2">
           <ShareIcon className="mr-2" />
-          <DeleteIcon />
+          <button className="hover:bg-gray-800" onClick={handleModalToggle}>
+            <DeleteIcon />
+          </button>
         </div>
       </div>
 
@@ -56,6 +72,8 @@ const Card: React.FC<CardProps> = ({ title, type, link, createdAt }) => {
       <p>
         Added on <u className="font-medium">{createdAt.toDateString()}</u>{" "}
       </p>
+
+      {modalOpen && <Modal id={contentId} handleClose={handleModalToggle} />}
     </div>
   );
 };
